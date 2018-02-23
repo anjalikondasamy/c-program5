16e41db9ead1a4caa2ae694ae4ab7e1193289c70
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-23 16:28:03 +0530

    c pgm
    good

---
+++ numericchatr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h.
+void main()
+{
+	int a[3]={1,2,3};
+  int i,count;
+  clrscr();
+  
+  for(i=0;i<=5;i++)
+  {
+  if(a!=null)
+  {
+  printf("count the numeric %d",a);
+  }
+  else
+  {
+  printf("present the line %d",a);
+  }
+  count++;
+  }
